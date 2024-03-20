ee4d451086293e00ae5a6a5dbcc3b26f80f70f10
dudleydehenau <skydrmddh@gmail.com>
2024-03-20 14:57:16 +0100

    redesign

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ function playAudio() {
     var audio = document.getElementById("myAudio");
     audio.play();
 }
+
+function toggleMenu() {
+    const sidebar = document.querySelector('.sidebar');
+    const menuIcon = document.querySelector('.menu-icon');
+
+    const isSidebarVisible = sidebar.classList.contains('show-sidebar');
+
+   
+    sidebar.classList.toggle('show-sidebar', !isSidebarVisible);
+
+    
+    if (!isSidebarVisible) {
+        menuIcon.style.transform = 'translateX(250px)';
+    } else {
+        menuIcon.style.transform = 'translateX(0)';
+    }
+}
